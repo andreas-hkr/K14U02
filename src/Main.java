fb29e0cafc7e31a18756d41e73ec197236510f5a
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 17:28:52 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,16 @@ public class Main {
         //   moveHorisontal = positiv parameter --> flytt till höger
         //   moveVertical   = positiv parameter --> flytt uppåt
 
+        Punkt p = new Punkt();
+        p.x = 5;
+        p.y = 3;
 
+        System.out.format("x=%.2f  y=%.2f%n", p.x, p.y);
+
+        p.moveHorisontal(3);
+        p.moveVertical(2);
+
+        System.out.format("x=%.2f  y=%.2f%n", p.x, p.y);
     }
 }
 
@@ -14,5 +23,13 @@ class Punkt {
 
     double distance() {
         return Math.sqrt(x*x + y*y);
+    }
+
+    void moveHorisontal(double d) {
+        x += d;
+    }
+
+    void moveVertical(double d) {
+        y += d;
     }
 }
